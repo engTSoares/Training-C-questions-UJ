9be77738cf3881e4c2881f10bc43e777b4a01dd5
Tiago Da Encarnação Soares <167035756+engTSoares@users.noreply.github.com>
2026-03-17 20:21:01 -0300

    Create Question14.c

---
+++ Question14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int numero;
+    printf("ALGORITMO ECO\n");
+    for(int i=0; i<5; i++){
+        printf("Digite um numero qualquer:");
+        scanf("%d",&numero);
+        printf("%d\n",numero);   
+    }        
+}
