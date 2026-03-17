445868b3eacb18da00111196dedd7b121bfa5cac
Tiago Da Encarnação Soares <167035756+engTSoares@users.noreply.github.com>
2026-03-17 11:29:41 -0300

    Create Question.c

---
+++ Question.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+float numUm, numDois, soma;
+printf("Digite um número:\n");
+scanf("%f",&numUm);
+printf("Digite outro número:\n");
+scanf("%f",&numDois);
+soma= numUm + numDois;
+printf("Soma: %2.f",soma);
+return 0;
+}
