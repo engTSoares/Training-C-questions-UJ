fae852b4d20a04a6cc58384f53fcb6d54cae0473
Tiago Da Encarnação Soares <167035756+engTSoares@users.noreply.github.com>
2026-03-17 11:17:53 -0300

    Create Question2.C

---
+++ Question2.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+printf("Os 5 primeiros numeros negativos:\n");
+for(int numNegativo=-1;  numNegativo > -6; numNegativo--){
+    printf("%d\n",numNegativo);
+}
+return 0;
+}
+
