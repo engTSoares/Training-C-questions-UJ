4e3908d2f3451b0a3cf91a951768a46bef4baa07
Tiago Da Encarnação Soares <167035756+engTSoares@users.noreply.github.com>
2026-03-17 12:04:03 -0300

    Update Question4.c

--- Question4.c
+++ Question4.c
@@ -7,6 +7,6 @@ scanf("%f",&numUm);
 printf("Digite outro número:\n");
 scanf("%f",&numDois);
 multiplicacao= numUm * numDois;
-printf("multiplicacao: %2.f",multiplicacao);
+printf("A multiplicacao de %2.f e %2.f: %2.f",numUm,numDois,multiplicacao);
 return 0;
 }
