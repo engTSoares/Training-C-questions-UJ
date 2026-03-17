7302d47674f80082d2584acee6230261eddcb16a
Tiago Da Encarnação Soares <167035756+engTSoares@users.noreply.github.com>
2026-03-17 19:11:09 -0300

    Create Question11.c

---
+++ Question11.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int qtdPositivos=0, qtdNegativos=0, qtdZeros=0, armazenamentoNumeros[5];
+    
+    //loop para adicionar numeros ao array
+    for(int j=0; j<5 ; j++){
+        printf("Insira um numero:\n");
+        scanf("%d",&armazenamentoNumeros[j]);
+    }
+
+    //loop para percorrer o array e contabilizar positivos, negativos  e zeros
+    for(int j=0; j<5 ; j++){
+        if(armazenamentoNumeros[j]==0){
+            qtdZeros++;
+        }else if(armazenamentoNumeros[j]>0){
+            qtdPositivos++;
+        }else if(armazenamentoNumeros[j]<0){
+            qtdNegativos++;
+        }
+    }
+
+    printf("Quantidade de zeros:%d\nQuantidade de negativos:%d\nQuantidade de positivos:%d\n",qtdZeros,qtdNegativos, qtdPositivos);
+
+}
