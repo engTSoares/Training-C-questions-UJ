fbc6608f49f93fa4a51c21a950d6bf36f75a3547
Tiago Da Encarnação Soares <167035756+engTSoares@users.noreply.github.com>
2026-03-17 11:32:04 -0300

    Create Question4.c

---
+++ Question4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+float numUm, numDois,multiplicacao;
+printf("Digite um número:\n");
+scanf("%f",&numUm);
+printf("Digite outro número:\n");
+scanf("%f",&numDois);
+multiplicacao= numUm * numDois;
+printf("multiplicacao: %2.f",multiplicacao);
+return 0;
+}
