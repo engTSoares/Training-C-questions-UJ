b875eeaa1144f3739677ef912d08c8190540abb3
Tiago Da Encarnação Soares <167035756+engTSoares@users.noreply.github.com>
2026-03-25 21:43:11 -0300

    Create Question18.c

---
+++ Question18.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Tabuada do numero 4:\n");
+    for(int i =0; i<11; i++){
+        int multi= 4 * i;
+        printf("4 x %d = %d",i, multi);
+        printf("\n");
+    }
+}
