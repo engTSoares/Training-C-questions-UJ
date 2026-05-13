6a4ea0efd72dd8fc0d3b750663710303bca01520
Tiago Da Encarnação Soares <167035756+engTSoares@users.noreply.github.com>
2026-05-12 23:57:41 -0300

    Update Question04.c

--- Question04.c
+++ Question04.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(){
-float numUm, numDois,multiplicacao;
-printf("Digite um número:\n");
-scanf("%f",&numUm);
-printf("Digite outro número:\n");
-scanf("%f",&numDois);
-multiplicacao= numUm * numDois;
-printf("A multiplicacao de %2.f e %2.f: %2.f",numUm,numDois,multiplicacao);
+  float numUm, numDois,multiplicacao;
+  printf("Digite um número:\n");
+  scanf("%f",&numUm);
+  printf("Digite outro número:\n");
+  scanf("%f",&numDois);
+  multiplicacao= numUm * numDois;
+  printf("A multiplicacao de %2.f e %2.f: %2.f",numUm,numDois,multiplicacao);
 return 0;
 }
